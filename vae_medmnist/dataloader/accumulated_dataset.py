0cf6057b496b08eca14eda218ed81bae31f74ce6
Pascal Kraus <pascal98kraus@gmail.com>
2024-06-19 20:47:51 +0200

    update v1.3.0 trained cvae, todo evaluation results

--- vae_medmnist/dataloader/accumulated_dataset.py
+++ vae_medmnist/dataloader/accumulated_dataset.py
@@ -7,13 +7,19 @@ Classes:
     AccumulatedMedMNIST: A class that inherits from LightningDataModule and handles the accumulation and management of multiple MedMNIST datasets.
 """
 
+import logging
 from argparse import ArgumentParser
 from typing import List
 
+import medmnist
+from medmnist import INFO
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader
 
-from vae_medmnist.datamodules.medmnist_datamodule import MedMNISTDataModule
+from vae_medmnist.dataloader.medmnist_datamodule import MedMNISTDataModule
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class AccumulatedMedMNIST(LightningDataModule):
@@ -43,13 +49,26 @@ class AccumulatedMedMNIST(LightningDataModule):
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
         """
-        super().__init__(*args, **kwargs)
-        self.datasets = datasets if datasets is not None else ['tissuemnist', 'octmnist', 'chestmnist']
+        super().__init__()
+        self.datasets = datasets if datasets is not None else ['tissuemnist', 'octmnist', 'organamnist']
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.size = size
         self.args = args
         self.kwargs = kwargs
+
+        self.infos = [INFO[dataset] for dataset in self.datasets]
+
+        self.in_channels = set(info['n_channels'] for info in self.infos)
+        assert len(self.in_channels) == 1, 'All datasets must have the same number of channels'
+
+        self.num_classes = sum(len(info['label']) for info in self.infos)
+        self.n_samples = {
+            'train': sum(info['n_samples']['train'] for info in self.infos),
+            'val': sum(info['n_samples']['val'] for info in self.infos),
+            'test': sum(info['n_samples']['test'] for info in self.infos),
+        }
+        self.labels = {}
 
     def setup(self, stage: str = None):
         """
@@ -58,12 +77,17 @@ class AccumulatedMedMNIST(LightningDataModule):
         Args:
             stage (str, optional): Stage for which setup is being called ('fit', 'validate', 'test', or 'predict'). Defaults to None.
         """
+        logger.info(f'Setting up {self.__class__.__name__} with {self.datasets} datasets')
         combined_datasets = []
         label_offset = 0
 
         for dataset in self.datasets:
+            info = INFO[dataset]
+            for _, label_name in info['label'].items():
+                self.labels[len(self.labels)] = label_name
+
             data_module = MedMNISTDataModule(
-                dataset_class=dataset,
+                dataset_class=getattr(medmnist, info['python_class']),
                 batch_size=self.batch_size,
                 num_workers=self.num_workers,
                 size=self.size,
@@ -71,20 +95,43 @@ class AccumulatedMedMNIST(LightningDataModule):
                 **self.kwargs,
             )
             data_module.setup(stage)
-            train_dataset = data_module.mnist_train
-            val_dataset = data_module.mnist_val
-            test_dataset = data_module.mnist_test
+
+            local_datasets = []
+            if stage == 'fit' or stage is None:
+                train_dataset = data_module.mnist_train
+                val_dataset = data_module.mnist_val
+                local_datasets.extend([train_dataset, val_dataset])
+
+            if stage == 'test' or stage is None:
+                test_dataset = data_module.mnist_test
+                local_datasets.append(test_dataset)
 
             # Adjust labels to ensure they are unique across different datasets
-            for dataset in [train_dataset, val_dataset, test_dataset]:
-                dataset.targets = dataset.targets + label_offset
+            for dataset in local_datasets:
+                if hasattr(dataset, 'labels'):
+                    dataset.labels = dataset.labels + label_offset
+                else:
+                    logger.error(f"{type(dataset).__name__} does not have a 'labels' attribute")
+                    raise AttributeError(f"{type(dataset).__name__} does not have a 'labels' attribute")
 
-            combined_datasets.extend([train_dataset, val_dataset, test_dataset])
-            label_offset += len(data_module.classes)
+            combined_datasets.extend(local_datasets)
+            label_offset += len(info['label'])
 
-        self.combined_train = ConcatDataset([ds for ds in combined_datasets if ds.split == 'train'])
-        self.combined_val = ConcatDataset([ds for ds in combined_datasets if ds.split == 'val'])
-        self.combined_test = ConcatDataset([ds for ds in combined_datasets if ds.split == 'test'])
+        if stage == 'fit' or stage is None:
+            self.combined_train = ConcatDataset([ds for ds in combined_datasets if ds.split == 'train'])
+            self.combined_val = ConcatDataset([ds for ds in combined_datasets if ds.split == 'val'])
+        if stage == 'test' or stage is None:
+            self.combined_test = ConcatDataset([ds for ds in combined_datasets if ds.split == 'test'])
+
+        if hasattr(self, 'combined_train'):
+            logger.info(f'Combined train dataset: {self.combined_train}')
+        if hasattr(self, 'combined_val'):
+            logger.info(f'Combined validation dataset: {self.combined_val}')
+        if hasattr(self, 'combined_test'):
+            logger.info(f'Combined test dataset: {self.combined_test}')
+        logger.info(f'Number of channels: {self.in_channels}')
+        logger.info(f'Number of classes: {self.num_classes}')
+        logger.info(f'Number of samples: {self.n_samples}')
 
     def train_dataloader(self):
         """
--- vae_medmnist/models/cvae.py
+++ vae_medmnist/models/cvae.py
@@ -6,57 +6,65 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.functional import one_hot
 
-from vae_medmnist.dataloader.medmnist_datamodule import MedMNISTDataModule
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s')
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class VAE(pl.LightningModule):
-    """ResNetVAE model class."""
-
+class CVAE(pl.LightningModule):
     def __init__(
         self,
         input_channels: int = 1,
-        lr: float = 1e-4,
+        num_classes: int = None,
+        lr: float = 1e-3,
         kl_coeff: float = 0.1,
-        hidden_channels: List = None,
-        latent_dim: int = 256,
+        latent_dim: int = 128,
+        hidden_channels: List[int] = None,
+        img_size: int = 28,
+        *args,  # noqa: ARG002
         **kwargs,  # noqa: ARG002
     ):
-        """Initialize the ResNetVAE model."""
         super().__init__()
         self.save_hyperparameters()
         self.input_channels = input_channels
+        self.num_classes = num_classes
         self.lr = lr
         self.kl_coeff = kl_coeff
-        self.hidden_channels = hidden_channels
         self.latent_dim = latent_dim
+        self.hidden_channels = hidden_channels
+        self.img_size = img_size
 
-        if self.hidden_channels is None:
-            self.hidden_channels = [32, 64, 128, 256, 512]
+        assert num_classes is not None, 'Number of classes must be provided'
+
+        if hidden_channels is None:
+            hidden_channels = [32, 64, 128, 256, 512]
+
+        self.embed_class = nn.Linear(num_classes, img_size * img_size)
+        self.embed_data = nn.Conv2d(input_channels, input_channels, kernel_size=1)
+
+        self.output_channels = self.input_channels
+        self.input_channels += 1
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(self.input_channels, self.hidden_channels[0], kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(self.hidden_channels[0]),
+            nn.Conv2d(self.input_channels, hidden_channels[0], kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(hidden_channels[0]),
             nn.ReLU(),
             *[
                 nn.Sequential(
-                    nn.Conv2d(self.hidden_channels[i], self.hidden_channels[i + 1], kernel_size=3, stride=2, padding=1),
-                    nn.BatchNorm2d(self.hidden_channels[i + 1]),
+                    nn.Conv2d(hidden_channels[i], hidden_channels[i + 1], kernel_size=3, stride=2, padding=1),
+                    nn.BatchNorm2d(hidden_channels[i + 1]),
                     nn.ReLU(),
                 )
-                for i in range(len(self.hidden_channels) - 1)
+                for i in range(len(hidden_channels) - 1)
             ],
         )
 
-        self.fc_mu = nn.Linear(self.hidden_channels[-1], self.latent_dim)
-        self.fc_logvar = nn.Linear(self.hidden_channels[-1], self.latent_dim)
+        self.fc_mu = nn.Linear(hidden_channels[-1], latent_dim)
+        self.fc_logvar = nn.Linear(hidden_channels[-1], latent_dim)
 
-        self.hidden_channels = self.hidden_channels[::-1]
-
-        self.decoder_input = nn.Linear(self.latent_dim, self.hidden_channels[0] * 4)
+        self.hidden_channels = hidden_channels[::-1]
+        self.decoder_input = nn.Linear(self.latent_dim + self.num_classes, self.hidden_channels[0] * 4)
 
         self.decoder = nn.Sequential(
             *[
@@ -77,58 +85,52 @@ class VAE(pl.LightningModule):
             nn.ConvTranspose2d(self.hidden_channels[-1], self.hidden_channels[-1], kernel_size=3, stride=2, padding=4),
             nn.BatchNorm2d(self.hidden_channels[-1]),
             nn.ReLU(),
-            nn.Conv2d(self.hidden_channels[-1], self.input_channels, kernel_size=3, stride=2),
+            nn.Conv2d(self.hidden_channels[-1], self.output_channels, kernel_size=3, stride=2),
             nn.Tanh(),
         )
 
-    def forward(self, x):
-        """Forward pass through the model."""
-        x = self.encoder(x)
-        res = torch.flatten(x, start_dim=1)
+    def forward(self, x, y):
+        y_flat = y.squeeze().to(torch.int64)
+        y_onehot = one_hot(y_flat, self.num_classes).to(torch.float32)
+        embedded_class = self.embed_class(y_onehot)
+        embedded_class = embedded_class.view(-1, self.img_size, self.img_size).unsqueeze(1)
+        embedded_data = self.embed_data(x)
+
+        encoder_input = torch.cat([embedded_data, embedded_class], dim=1)
+        res = self.encoder(encoder_input)
+        res = torch.flatten(res, start_dim=1)
 
         mu = self.fc_mu(res)
         logvar = self.fc_logvar(res)
 
         z = self.reparameterize(mu, logvar)
 
-        return (
-            self.decode(z),
-            mu,
-            logvar,
-        )
+        return (self.decode(z, y_onehot), mu, logvar)
 
-    def decode(self, x):
-        """Decode the latent variables."""
-        decoder_input = self.decoder_input(x)
+    def decode(self, z, y_onehot):
+        concat = torch.cat([z, y_onehot], dim=1)
+        decoder_input = self.decoder_input(concat)
         decoder_input = decoder_input.view(-1, self.hidden_channels[0], 2, 2)
 
         return self.decoder(decoder_input)
 
     def reparameterize(self, mu, logvar):
-        """Reparameterize the latent variables."""
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
-        z = mu + eps * std
-
-        return z
+        return mu + eps * std
 
     def step(self, batch, batch_idx):  # noqa: ARG002
-        """Perform a single optimization step."""
         x, y = batch
-        x_hat, mu, logvar = self(x)
+        x_hat, mu, logvar = self(x, y)
 
         recon_loss = F.mse_loss(x_hat, x)
 
         kl = torch.mean(-0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar), dim=1), dim=0)
         kl *= self.kl_coeff
+
         loss = recon_loss + kl
 
-        logs = {
-            'recon_loss': recon_loss,
-            'kl': kl,
-            'loss': loss,
-        }
-
+        logs = {'loss': loss, 'recon_loss': recon_loss, 'kl': kl}
         return loss, logs
 
     def training_step(self, batch, batch_idx):
@@ -147,12 +149,14 @@ class VAE(pl.LightningModule):
         """Configure the optimizers."""
         return torch.optim.Adam(self.parameters(), lr=self.lr)
 
-    def sample(self, num_samples=1, device=None):
-        """Sample new pictures from the VAE."""
+    def sample(self, num_samples: int, class_idx: int, device=None):
         device = device or self.device
-        z = torch.randn(num_samples, self.latent_dim, device=device)
-        samples = self.decode(z)
-        return samples
+        y_onehot = one_hot(class_idx, self.num_classes)
+        z = torch.randn(num_samples, self.latent_dim).to(device)
+
+        z = torch.cat([z, y_onehot], dim=1)
+
+        return self.decode(z, y_onehot)
 
     @staticmethod
     def add_model_specific_args(parent_parser):
@@ -171,12 +175,11 @@ class VAE(pl.LightningModule):
 
 
 def cli_main(args=None):
-    """Main function for command-line interface."""
     import yaml
-    from medmnist import INFO
-    from medmnist.dataset import TissueMNIST
     from pytorch_lightning.callbacks import ModelCheckpoint
     from pytorch_lightning.loggers import CSVLogger
+
+    from vae_medmnist.dataloader.accumulated_dataset import AccumulatedMedMNIST
 
     parser = ArgumentParser()
     parser.add_argument('--config', type=str, help='Path to the configuration file')
@@ -189,32 +192,29 @@ def cli_main(args=None):
         logger.debug(f'Loaded configuration: {config}')
 
     # Update parser with options from the config file
-    parser = VAE.add_model_specific_args(parser)
+    parser = CVAE.add_model_specific_args(parser)
     parser.set_defaults(**config)
     args = parser.parse_args(unknown)
 
-    if args.dataset == 'tissuemnist':
-        dataset_class = TissueMNIST
+    if isinstance(args.datasets, list):
+        datamodule = AccumulatedMedMNIST(**args.__dict__)
+        args.input_channels = next(iter(datamodule.in_channels))
+        args.num_classes = datamodule.num_classes
     else:
         raise ValueError(f'Unknown dataset: {args.dataset}')
 
-    datamodule = MedMNISTDataModule(dataset_class, **args.__dict__)
-    args.input_channels = INFO[args.dataset]['n_channels']
+    model = CVAE(**args.__dict__)
 
-    model = VAE(**args.__dict__)
-
-    # Setup model checkpointing
     checkpoint_callback = ModelCheckpoint(
         dirpath=args.checkpoint_dir,
-        filename='vae-best-checkpoint',
+        filename='cvae-best-checkpoint',
         save_top_k=1,
         verbose=True,
         monitor='val_loss',
         mode='min',
     )
 
-    # Setup CSV logging
-    csv_logger = CSVLogger(save_dir=args.log_dir, name='training_logs')
+    csv_logger = CSVLogger(args.log_dir, name='training_logs')
 
     trainer = pl.Trainer(max_epochs=args.max_epochs, callbacks=[checkpoint_callback], logger=csv_logger)
     trainer.fit(model, datamodule=datamodule)
